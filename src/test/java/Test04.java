5a0d482204fd0534896c64c59c3efc55d9af2d04
BingCoke <2486773105@qq.com>
2021-07-25 14:54:36 +0800

    dd2222

--- src/test/java/Test04.java
+++ src/test/java/Test04.java
@@ -1,2 +1,7 @@
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+
 public class Test04 {
+    public static void main(String[] args) {
+        System.out.println(111111111);
+    }
 }
