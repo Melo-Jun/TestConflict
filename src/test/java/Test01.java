a60e83281850cb740b30258e21fd89eef0bd9098
JiangLianghao <2429890953@qq.com>
2021-07-25 13:54:16 +0800

    "提交"

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -4,6 +4,7 @@ public class Test01 {
 
     @Test
     public void test01(){
-        System.out.println(2222);
+        System.out.println(24124214);
+        System.out.println(3333);
     }
 }
--- src/test/java/Test03.java
+++ src/test/java/Test03.java
@@ -4,12 +4,12 @@ import org.junit.Test;
  * @author JLHWASX   Email:2429890953@qq.com
  * @Description
  * @verdion
- * @date 2021/7/2513:36
+ * @date 2021/7/2513:37
  */
 
-public class Test02 {
+public class Test03 {
     @Test
-    public void test02(){
-        System.out.println(33);
+    public void test03(){
+        System.out.println(44);
     }
 }
